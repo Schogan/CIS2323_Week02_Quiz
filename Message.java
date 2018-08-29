f63f165c4c33e4f2893aa23537f455eb7e2f8965
Schogan <navy15@gmail.com>
2018-08-29 17:58:05 -0500

    complete

---
+++ Message.java
@@ -0,0 +1,9 @@
+public class Message {
+	public static void main(String[] args) {
+		System.out.println("Logan Ray Schanbacher");
+		System.out.println("");
+		System.out.println("I learned how to define a preferred size of my JFrame");
+		System.out.println("in order to have the business card display properly without");
+		System.out.println("the user needing to resize the frame manually.");
+	}
+}
